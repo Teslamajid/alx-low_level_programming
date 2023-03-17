6f814171053c298aa5d2c4568126f7379a3ca251
Teslamajid <bahabdulmajid775@gmail.com>
2023-03-17 14:30:27 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include ,studio.h>
+
+
+/**
+ * main - prints "with proper grammer, but the outcome is a 
+ * piece of art." followed newline
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of ar.\n");
+        return (0);
+}
