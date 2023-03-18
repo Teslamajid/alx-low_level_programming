b3e8bb71e4cbc556b89c39c6f26a36efc157f89f
Teslamajid <bahabdulmajid775@gmail.com>
2023-03-18 16:30:13 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@
  */
 int main(void)
 {
-	write(2, "andthatpieceofartisuseful\" -DoraKorpar, 2015-10-19\nstdout", 59);
+write(2, "and that piece of art is useful\" -Dora Korpar,2015-10-19\nstdout", 59);
 	return (1);
 }
