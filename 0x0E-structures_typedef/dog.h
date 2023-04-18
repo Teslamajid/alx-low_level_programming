55d2bd023dab2abd021c002051c900819596038b
Teslamajid <bahabdulmajid775@gmail.com>
2023-04-18 15:49:28 -0700

    README.md

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
 
+
 /**
- * struct dog - a dog's basic info
+ * struct dog - dog's basic info
  * @name: First member
  * @age: Second member
  * @owner: Third member
  *
  * Description: Longer description
  */
+
 struct dog
 {
 	char *name;
@@ -19,11 +21,12 @@ struct dog
 /**
  * dog_t - typedef for struct dog
  */
-	typedef struct dog dog_t;
-	void init_dog(struct dog *d, char *name, float age, char *owner);
-	void print_dog(struct dog *d);
-	dog_t *new_dog(char *name, float age, char *owner);
-	void free_dog(dog_t *d);
-	char *_strcpy(char *dest, char *src);
-	int _strlen(char *s);
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 #endif
